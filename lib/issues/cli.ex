a67994fb171875e136b43cccebfc81f86681f851
deby22 <dawid.deby@gmail.com>
2021-03-11 14:57:22 +0100

    cli, github_issues, add dependency

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,7 +1,31 @@
 defmodule Issues.CLI do
   @default_count 4
 
-  def run(argv), do: parse_args(argv)
+  def run(argv) do
+    argv
+    |> parse_args
+    |> process
+  end
+
+  def process(:help) do
+    IO.puts("""
+      usage: issues <user> <project> [count | #{@default_count} ]
+    """)
+
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
+  end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    IO.puts("Error fetching from Github: #{error["message"]}")
+    System.halt(2)
+  end
 
   def parse_args(argv) do
     OptionParser.parse(argv,
